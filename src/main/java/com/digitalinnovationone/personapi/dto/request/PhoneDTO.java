a0b77da6cf62a95496b4b071438384f261130354
Luiz Santos <luizcsdev@gmail.com>
2021-06-27 14:15:54 -0300

    Add files dto

---
+++ src/main/java/com/digitalinnovationone/personapi/dto/request/PhoneDTO.java
@@ -0,0 +1,28 @@
+package com.digitalinnovationone.personapi.dto.request;
+
+import com.digitalinnovationone.personapi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhoneDTO {
+
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PhoneType type;
+
+    @NotEmpty
+    @Size(min = 13, max = 14)
+    private String number;
+}
